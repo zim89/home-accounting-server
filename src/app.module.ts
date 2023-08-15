81a28f61e2f7626d50460761cd672e1fa696f294
zim89 <121241841+zim89@users.noreply.github.com>
2023-08-15 06:40:47 +0300

    add auth, user, category, transaction modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { CategoryModule } from './category/category.module';
+import { AuthModule } from './auth/auth.module';
+import { TransactionModule } from './transaction/transaction.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, CategoryModule, AuthModule, TransactionModule],
   controllers: [AppController],
   providers: [AppService],
 })
